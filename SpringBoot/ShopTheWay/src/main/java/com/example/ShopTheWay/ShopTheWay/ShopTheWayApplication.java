88a206d4493a44c69b6255c0cac836b16975a3e9
CodingBlood <kartik.agarwal.192.168@gmail.com>
2022-04-08 09:53:22 +0530

    Added in SpringBoot API

---
+++ SpringBoot/ShopTheWay/src/main/java/com/example/ShopTheWay/ShopTheWay/ShopTheWayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShopTheWay.ShopTheWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopTheWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopTheWayApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/ShopTheWay/src/test/java/com/example/ShopTheWay/ShopTheWay/ShopTheWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopTheWay.ShopTheWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopTheWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
